1457c9972fffc61eb809843f6086dc8c34d7a92b
Tianyang Joe Qiao <qiaoty@outlook.com>
2019-01-25 23:13:39 -0800

    Bug fix

--- Concrete01.m
+++ Concrete01.m
@@ -33,12 +33,15 @@ epscu = MatData(1,5);      % concrete strain at crushing strength (negative)
 % trial variables
 stressT = MatData(1,6);
 strainT = MatData(1,7);
+tangentT = MatData(1,8);
 % state history variables
-stressC = MatData(1,8);
-strainC = MatData(1,9);
-strainCmin = MatData(1,10);
-strainCunload = MatData(1,11);
-strainCend = MatData(1,12);
+stressC = MatData(1,9);
+strainC = MatData(1,10);
+strainCmin = MatData(1,11);
+strainCunload = MatData(1,12);
+strainCend = MatData(1,13);
+unloadSlopeC = MatData(1,14);
+tangentC = MatData(1,15);
 
 % Force all parameters negative
 if fpc > 0
@@ -56,9 +59,6 @@ end
 
 % Initial values
 Ec0 = 2*fpc/epsc;
-tangentC = Ec0;
-unloadSlopeC = Ec0;
-tangentT = Ec0;
 
 switch action
    % ======================================================================
@@ -70,12 +70,15 @@ switch action
        strainCmin = 0;
        strainCunload = 0;
        strainCend = 0;
+       tangentC = Ec0;
+       unloadSlopeC = Ec0;
+       tangentT = Ec0;
        Result = 0;
        
    % ======================================================================
    case 'setTrialStrain'
        strainT = edp;
-       Result = 0;  
+       Result = 0;           
         
    % ======================================================================
    case 'setTrialStress'
@@ -87,33 +90,90 @@ switch action
        if stressT >= 0
            Result = strainT;
        else
-
+           Result = strainT;  % stub
        end
       
    % ======================================================================
    case 'getStress'
+       % Determine change in strain from last state
+       dStrain = strainT - strainC;
+       % Reset trail history variables to last committed state
+       strainTmin = strainCmin;
+       strainTend = strainCend;
+       unloadSlopeT = unloadSlopeC;
+       stressTemp = stressC + unloadSlopeT*dStrain;
        if strainT > 0
-           Result = 0;
+           % Quick output when strain>0
+            Result = 0;
        else
-           % determine trial variables given the change in strain
-           dStrain = strainT - strainC;
-           unloadSlopeT = unloadSlopeC;
-           stressTemp = stressC + unloadSlopeT*strainT - unloadSlopeT*strainC;
-           if strainT < strainC     % Goes further into compression
-               strainTmin = strainCmin;
-               strainTend = strainCend;
-               if stressTemp > stressT
-                   stressT = stressTemp;
-                   tengentT = unloadSlopeT;
-               end
-           elseif stressTemp <= 0
-               stressT = stressTemp;
-               tangentT = unloadSlopeT;
-           else
-               stressT = 0;
-               tangentT = 0;
-           end
+            % Material goes into compression
+            if strainT <= strainC
+                % -----Reload -----
+                if strainT <= strainTmin
+                    strainTmin = strainT;
+                    % ----- Envelope -----
+                    if strainT > epsc
+                        eta = strainT/epsc;
+                        stressT = fpc*(2*eta-eta*eta);
+                        tangentT = Ec0*(1-eta);
+                    elseif strainT > epscu
+                        tangentT = (fpc-fpcu)/(epsc-epscu);
+                        stressT = fpc+tangentT*(strainT-epsc);
+                    else
+                        tangentT = 0;
+                        stressT = fpcu;
+                    end
+                    % ----- End of Envelope -----
+                    % ----- Unload -----
+                    strainTemp = strainTmin;
+                    if strainTemp < epscu
+                        strainTemp = epscu;
+                    end
+                    eta = strainTemp/epsc;
+                    ratio = 0.707*(eta-2)+0.834;
+                    if eta < 2
+                        ratio = 0.145*eta*eta+0.13*eta;
+                    end
+                    strainTend = ratio*epsc;
+                    temp1 = strainTmin - strainTend;
+                    temp2 = stressT/Ec0;
+                    if temp1 > 0
+                        unloadSlopeT = Ec0;
+                    elseif temp1 <= temp2
+                        strainTend = strainTmin - temp1;
+                        unloadSlopeT = stressT/temp1;
+                    else
+                        strainTend = strainTmin - temp2;
+                        unloadSlopeT = Ec0;
+                    end
+                    % ----- End of Unload -----
+                elseif strainT <= strainTend
+                    tangentT = unloadSlopeT;
+                    stressT = tangentT*(strainT-strainTend);
+                else
+                    tangentT = 0;
+                    stressT = 0;
+                end
+                % ----- End of Reload -----
+                
+                if stressTemp > stressT
+                    stressT = stressTemp;
+                    tangentT = unloadSlopeT;
+                end
+                
+            elseif stressTemp <= 0
+                stressT = stressTemp;
+                tangentT = unloadSlopeT;
+            else
+                stressT = 0;
+                tangentT = 0;                
+            end
+            Result = stressT;
        end
+       % History variables output
+       strainCmin = strainTmin;
+       unloadSlopeC = unloadSlopeT;
+       strainCend = strainTend;
       
    % ======================================================================
    case 'getFlexibility'
@@ -133,14 +193,18 @@ switch action
       
    % ======================================================================
    case 'getInitialStiffness'
-       Result = E;
+       Result = Ec0;
       
    % ======================================================================
    case 'getInitialFlexibility'
-       Result = 1/E;
+       Result = 1/Ec0;
         
    % ======================================================================
    case 'commitState'
+       % State variables
+       strainC = strainT;
+       stressC = stressT;
+       tangentC = tangentT;
        Result = 0;
       
    % ======================================================================
@@ -155,7 +219,13 @@ MatData(1,5) = epscu;
 % trial variables
 MatData(1,6) = stressT;
 MatData(1,7) = strainT;
+MatData(1,8) = tangentT;
 % state history variables
-MatData(1,8) = stressC;
-MatData(1,9) = strainC;
+MatData(1,9) = stressC;
+MatData(1,10) = strainC;
+MatData(1,11) = strainCmin;
+MatData(1,12) = strainCunload;
+MatData(1,13) = strainCend;
+MatData(1,14) = unloadSlopeC;
+MatData(1,15) = tangentC;
 end
